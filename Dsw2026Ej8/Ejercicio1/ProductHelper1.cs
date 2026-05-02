d6c6781976d02ef809bfa602134e71b164d087d3
Enzo Leonel Gutierrez <enzo.gutierrez@alu.frt.utn.edu.ar>
2026-05-02 20:09:56 -0300

    Ejercicio 1 Resuelto

---
+++ Dsw2026Ej8/Ejercicio1/ProductHelper1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio1
+{
+    internal partial class ProductHelper
+    {
+
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            // Usa el método auxiliar para formatear el precio
+            string precioFormateado = FormatearPrecio(price);
+
+            // Devuelve la etiqueta con el formato solicitado
+            return $"[{code}] {description} - {precioFormateado}";
+        }
+    }
+
+
+}
+
---
+++ Dsw2026Ej8/Ejercicio1/ProductHelper2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio1
+{
+    internal partial class ProductHelper
+    {
+     
+        // Método auxiliar para dar formato de moneda
+        private string FormatearPrecio(decimal price)
+        {
+            
+            CultureInfo cultura = new CultureInfo("es-AR");
+            return price.ToString("C", cultura);
+        }
+    }
+
+}
+
