c51f58fe240e133b5f91208d49aa94cab905fea2
Enzo Leonel Gutierrez <enzo.gutierrez@alu.frt.utn.edu.ar>
2026-05-02 22:27:52 -0300

    Ejercicio6 Resuelto

---
+++ Dsw2026Ej8/Ejercicio6/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Ejercicio6
+{
+    internal static class Extensions
+    {
+        public static string ToProductCode(this string code)
+        {
+            if (code == null)
+                return "SIN-CODIGO";
+
+            // Eliminar espacios al inicio y al final
+            string resultado = code.Trim();
+
+            // Convertir a mayúsculas
+            resultado = resultado.ToUpper();
+
+            // Reemplazar espacios internos por guiones
+            resultado = resultado.Replace(" ", "-");
+
+            return resultado;
+
+        }
+    }
+}
